5891bdc402fc13a30e0abdf08df5b3a2f4e059ce
anjalirana11 <anjaliranacse@gmail.com>
2022-01-04 21:59:29 +0530

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Anjali' },
+  { id: 12, name: 'Adi' },
+  { id: 13, name: 'Sam' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Reena' },
+  { id: 16, name: 'Ram' },
+  { id: 17, name: 'Sham' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
